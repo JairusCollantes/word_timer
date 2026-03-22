7a6aa00e347b81b45aac5559b5510e7f2665ad78
Jairus Mark <jairuscollantes0@gmail.com>
2026-03-22 09:29:13 +0800

    yessir

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+while True:
+    target_word = "hello"
+
+    print(f"Type the word: {target_word}")
+    print("Timer starts when you enter the first letter and ends when you enter the letter.\n")
+
+    typed = ""
+    start_time = None
+
+    while True:
+        if typed.strip() == target_word:
+            break
+        char = input("Enter next character: ")
+
+        if not char:
+            continue
+        if typed == "":
+            char = char[0]
+
+        if start_time is None:
+            start_time = time.time()
+
+        if char == " ":
+            end_time = time.time()
+            break
+
+        typed += char
+    end_time = time.time()
+    elapsed = end_time - start_time
+
+    print("\n--- Result ---")
+    print(f"Your input: {typed}")
+    print(f"Target word: {target_word}")
+    print(f"Time taken: {elapsed:.3f} seconds")
+
+    if typed.strip() == target_word:
+        print("Correct!")
+    else:
+        print("Incorrect.")
